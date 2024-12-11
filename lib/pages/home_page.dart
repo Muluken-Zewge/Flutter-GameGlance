97686a3f58ee2d6e91a438900afc16d66a267dc5
Muluken Zewge <mulukenzewge@gmail.com>
2024-12-11 17:05:10 +0300

    create a custom HomePage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gameglance/data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  dynamic _deviceHeight;
+  dynamic _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
