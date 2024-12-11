401ce22359171c5ef5c5073fe898119166ac8856
Muluken Zewge <mulukenzewge@gmail.com>
2024-12-11 17:09:17 +0300

    add a PageView with scrollable game pics on the HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,6 +14,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [_featuredGamesWidget()],
+      ),
+    );
+  }
+
+  Widget _featuredGamesWidget() {
+    return SizedBox(
+        height: _deviceHeight * 0.5,
+        width: _deviceWidth,
+        child: PageView(
+          scrollDirection: Axis.horizontal,
+          children: featuredGames.map((game) {
+            return Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(game.coverImage.url))),
+            );
+          }).toList(),
+        ));
   }
 }
